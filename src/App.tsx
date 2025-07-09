cce8b8ae13b5459e6a4ab9cb476f213a965244c7
Israel Hangy <israelngeve950@gmail.com>
2025-07-09 04:02:57 -1100

    feat: a new feature has been added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+   <div>
+    <div className="p-5 md:px-[8%]">
+      <Navbar />
+    </div>
+   </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import { Container } from "lucide-react"
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-center md:justify-between items-center p-4 ">
+            <a href="#"
+                className="flex items-center font-bold text-xl md:text-2xl">
+                <Container className="mr-2" />
+                BAKI
+                <span className="text-warning">DEV</span>
+            </a>
+            <ul className="hidden md:flex space-x-4">
+                <li>
+                    <a href="#" className="btn btn-lg btn-ghost">Accueil</a>
+                </li>
+                <li>
+                    <a href="#" className="btn btn-lg btn-ghost">A propos</a>
+                </li>
+                <li>
+                    <a href="#" className="btn btn-lg btn-ghost">Mes Expériences</a>
+                </li>
+                 <li>
+                    <a href="#" className="btn btn-lg btn-ghost">Mes Projets</a>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
