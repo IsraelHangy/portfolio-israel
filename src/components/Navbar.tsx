a43a5554686381c7577b8b0faf9da27decdf4fa9
Israel Hangy <israelngeve950@gmail.com>
2025-08-15 15:30:38 -1100

    docs: change have been to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,17 @@ const Navbar = () => {
     <div className="md:fixed md:top-0 md:left-0 md:w-full z-50 bg-base-100">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-2 md:p-4">
         <a href="#" className="flex items-center font-extrabold text-xl md:text-2xl">
-          <Flame className="mr-2 w-[30px] h-[30px] md:w-[40px] md:h-[40px]" strokeWidth={2} fill="white" color="white" />
-          BAKI<span className="font-extrabold text-red-500">DEV</span>
+          <div className="flex items-center gap-1">
+            <Flame
+              className="w-[30px] h-[30px] md:w-[40px] md:h-[40px]"
+              strokeWidth={2}
+              fill="white"
+              color="white"
+            />
+            <p className="font-poppins font-extrabold text-xl md:text-2xl">
+              BAKI<span className="text-red-500 pr-5">DEV</span>
+            </p>
+          </div>
         </a>
 
         {/* Desktop menu */}
@@ -35,30 +44,43 @@ const Navbar = () => {
           ))}
         </ul>
 
-        {/* Hamburger mobile */}
+        {/* Hamburger mobile à deux traits */}
         <div className="md:hidden">
           <button
             onClick={() => setMenuOpen(!menuOpen)}
-            className="flex flex-col justify-center items-center w-8 h-8 space-y-1"
+            className="flex flex-col justify-center items-center w-10 h-10 gap-1 p-2"
           >
-            <span className={`block w-8 h-0.5 bg-black transition-transform ${menuOpen ? "rotate-45 translate-y-2" : ""}`}></span>
-            <span className={`block w-8 h-0.5 bg-black transition-opacity ${menuOpen ? "opacity-0" : ""}`}></span>
-            <span className={`block w-8 h-0.5 bg-black transition-transform ${menuOpen ? "-rotate-45 -translate-y-2" : ""}`}></span>
+            <span
+              className={`block w-8 h-2 bg-white transform transition-all duration-300 ease-in-out ${
+                menuOpen ? "rotate-45 translate-y-2" : ""
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-2 bg-white transform transition-all duration-300 ease-in-out ${
+                menuOpen ? "-rotate-45 -translate-y-2" : ""
+              }`}
+            ></span>
           </button>
         </div>
       </div>
 
       {/* Mobile menu */}
-      {menuOpen && (
-        <ul className="md:hidden flex flex-col space-y-2 p-4 font-poppins bg-base-100">
+      <div
+        className={`md:hidden overflow-hidden transition-all duration-500 ${
+          menuOpen ? "max-h-96" : "max-h-0"
+        }`}
+      >
+        <ul className="flex flex-col space-y-2 p-4 font-poppins bg-base-100">
           {links.map((link) => (
             <li key={link.name}>
               <a
                 href={link.href}
-                className={`btn btn-lg btn-ghost w-full text-left ${active === link.name ? "text-red-500" : ""}`}
+                className={`btn btn-lg btn-ghost w-full text-left ${
+                  active === link.name ? "text-red-500" : ""
+                }`}
                 onClick={() => {
                   setActive(link.name);
-                  setMenuOpen(false); 
+                  setMenuOpen(false);
                 }}
               >
                 {link.name}
@@ -66,7 +88,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-      )}
+      </div>
     </div>
   );
 };
